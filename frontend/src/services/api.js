c7b80a3902319fb1745b83894fd1c094df8e46e4
natansevero <natanseverolp@gmail.com>
2020-04-11 14:33:33 -0300

    Finanlizando ajustes de deploy para heroku e config de docker

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+let baseURL = 'http://localhost:3333/api/v1';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333/api/v1'
+    baseURL
 });
 
 export default api;
